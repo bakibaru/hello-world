d49ec145351e5fecb46c7d0c497ceb6f9fc8f599
bakibaru <87686791+bakibaru@users.noreply.github.com>
2022-02-15 15:25:12 +0900

    Add files via upload

---
+++ ec3/common3.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+
+  function connect() {
+    return new PDO("mysql:dbname=ecshop3", "root");
+  }
+
+  function img_tag($code) {
+    if (file_exists("img/$code.jpg")) $name = $code;
+    else $name = 'noimage';
+    return '<img src="img/' . $name . '.jpg" alt="">';
+  }
+?>
